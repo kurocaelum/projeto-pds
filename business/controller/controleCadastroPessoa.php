6ac25a972a911b1e85743852f17967f6bb1f5976
douglasboza <douglasboza@gmail.com>
2019-08-29 11:41:06 -0300

    Controle pessoa

--- business/controller/controleCadastroPessoa.php
+++ business/controller/controleCadastroPessoa.php
@@ -1,25 +1,45 @@
 <?php 
 include("Pessoa.php");
+include("../../data/pessoaDAO.php");
 
 class ControleCadastroPessoa{
-	public $pessoa;
+	$pessoaDAO = new PessoaDAO(); // objeto dao para salvar as pessoas e obter dados.
+	$pessoasArray = []; //lista de todas as pessoas
 
-	public function __construct($pessoa){
-        $this->pessoa = $pessoa;
+	public function __construct(){
+
     }
 
 	function getIdCadastroPessoa(){
 		return 'asdsad';
-    	// return $this->idFuncionario;
     }
 
-    // fazer get e set pessoa
+    function addPessoa($pessoa){
+    	//envia o objeto pessoa para a funcao salvar pessoa no PessoaDAO
+    	$pessoaDAO->salvarPessoa($pessoa); //criar esse método para salvar a pessoa no banco de dados
+    }
+
+    function getListaPessoas(){
+    	//método para retonar o arraylist de pessoas
+    	$this->$pessoasArray = $pessoaDAO->getPessoasArray();//criar ess método para retornar todas as pessoas no DAO
+    	return $pessoasArray;
+    }
+    
 	
 }
 
 if(isset($_POST['addPessoa'])){
+	$nomePessoa = $_POST['nomePessoa'];
+	$pessoa = new Pessoa();
+	$pessoa->setNome($nomePessoa);
+
 	$controleCadastroPessoa = new ControleCadastroPessoa();
-	// pegar demais dados enviados do form html
+	$controleCadastroPessoa->addPessoa($pessoa);
+
+
+	// pegar demais dados enviados do form html e adicionar uma pesssoa
+
+
 }
 
 
